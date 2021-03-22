6a7b4fac9ac6809fbf2b76c3ced3d2bb27f71aec
hhuumm <h4mid@Hotmail.com>
2021-03-22 16:48:00 -0400

    Janea and Alejandra FTW

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,34 +3,42 @@ const Flight = require('../models/flight')
 module.exports={
 index,
 create,
-new:newFlight
+new:newFlight,
+show
 
 
 }
 
-
+function show(req, res){
+  Flight.findById(req.params.id)
+      res.render('flights/show', {
+        title: 'Flight Detail',
+        flight 
+        
+      })
+    }
 function index(req, res) {
     // Find all the movies
     Flight.find({}, function(err, flights){
       // Render index view
       console.log('flights', flights)
       res.render('flights/index', {
-        Flights: flights,
+        flights,
         title: 'All Flights'
       })
     })
   }
   function newFlight(req, res) {
-    const flight = new Flight(req.body);
-    res.render('flights/new', {title: 'Add Flight',Flight: flight})
+    // const flight = new Flight(req.body);
+    res.render('flights/new', {title: 'Add Flight'})
   }
 
   function create(req, res) {
     // remove whitespace next to commas
-   
+    const flight = new Flight(req.body);
     flight.save(function(err) {
       // one way to handle errors
-      if (err) return res.render('movies/new');
+      if (err) return res.render('flights/new');
       console.log(flight);
       // for now, redirect right back to new.ejs
       res.redirect(`/flights/${flight._id}`);
--- models/flight.js
+++ models/flight.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline:String,
-    airport:{type:String,default:"DEN"},
-    flightNo:Number,
+    airline:{type:String,enum:['American','Southwest','United']},
+    airport:{type:String,enum:['AUS','DFW','DEN','LAX','SAN'],default:"DEN"},
+    flightNo:{type:Number,min: 10, max: 9999},
     departs:{type:Date, default: () => Date.now() + 7*24*60*60*1000},
     })
 
